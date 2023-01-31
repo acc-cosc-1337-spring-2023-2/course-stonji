575b6173adcc009688b6a2b9727bb78e4df7a1d7
stonji <stonji@me.com>
2023-01-31 16:36:57 -0600

    jan31 commit

--- src/examples/02_expressions/01_expressions/main.cpp
+++ src/examples/02_expressions/01_expressions/main.cpp
@@ -1,5 +1,17 @@
+#include<iostream>
+
+#include std::cout;
 
 int main()
 {
+    auto num1 = 0, num2=0, num3=0;
+    
+    cout<<"Enter value for num1: ";
+    cin>>num1;
+
+    cout<<"enter value for num2: ";
+    cin>>num2;
+
+    cout<<"Enter value for num3: ";
     return 0;
 }
--- test/examples/02_expressions/02_expressions_tests.cpp
+++ test/examples/02_expressions/02_expressions_tests.cpp
@@ -1,6 +1,20 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "expressions.h"
+#include "casting.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
+}
+
+TEST_CASE("Verify operator_precedence_1", "validation order of operations") {
+	REQUIRE(operator_precedence_1(3, 6, 3) == 5);
+}
+
+TEST_CASE("Verify operator_precedence_2", "validation use of parenthesis") {
+	REQUIRE(operator_precedence_2(3, 6, 3) == 3);
+}
+
+TEST_CASE("Verify convert int to double", "int converted to double") {
+	REQUIRE(convert_to_double(10, .5) == 5);
 }
