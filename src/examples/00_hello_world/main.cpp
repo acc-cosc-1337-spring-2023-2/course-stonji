33da37b37aa18606593f0f99d29be295f3758c7c
stonji <stonji@me.com>
2023-02-11 13:23:37 -0600

    homework 2

--- src/examples/00_hello_world/main.cpp
+++ src/examples/00_hello_world/main.cpp
@@ -1,9 +1,8 @@
 #include<iostream> //tells c++ to bring in the code that allows us to display to screen
 
-using std::cout;
 
 int main()
 {
-    cout<<"Hello World!";
+    std::cout<<"Hello World!";
     return 0;
 }
