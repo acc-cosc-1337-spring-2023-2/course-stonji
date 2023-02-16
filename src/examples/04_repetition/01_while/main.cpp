de92770e6b116a26035b3f1d9962ad74aa8d0dfc
stonji <stonji@me.com>
2023-02-16 16:16:02 -0600

    2142023

--- src/examples/04_repetition/01_while/main.cpp
+++ src/examples/04_repetition/01_while/main.cpp
@@ -1,7 +1,18 @@
 #include "while.h"
 
+using std::string;
+using std::cin; using std::cout;
+
 int main() 
 {
+	auto num = 0;
+	cout<<"Enter a number: ";
+	cin>>num;
+
+	auto result = num
+
+	//string str = "Hello";
+	//display(str);
 
 	return 0;
 }
