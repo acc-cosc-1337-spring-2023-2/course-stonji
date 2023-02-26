09510df8d3f73ba8e36b3d2161346527fa8d0081
stonji <stonji@me.com>
2023-02-26 17:45:30 -0600

    fractorial and gcd assignment

--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,7 +1,7 @@
 //write include statements
-
+#include "dna.h"
 //write using statements
-
+using std::cout; using std::cin;
 
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
@@ -12,5 +12,51 @@ user enters a y or Y.
 */
 int main() 
 {
+	auto num = 0;
+	auto option = 0;
+	do
+    {
+        cout<<"\n\n\tMAIN MENU\n";
+		cout<<"1 - Factorial\n";
+		cout<<"2 - Greatest Common Divisor\n";
+		cout<<"3 - Exit\n";
+        cout<<"\nEnter menu option: ";
+        cin>>option;
+        
+		if(option == 1)
+		{
+			cout<<"Enter a number to get the fractorial: \n";
+			cin>>num;
+			
+			cout<<"\nThe factorial of "<<num<<" is: "<<factorial(num);
+		}
+		else if(option == 2)
+		{
+			auto num1 = 0;
+			auto num2 = 0;
+			cout<<"Enter two numbers to get the Greatest Common Divisor: \n";
+			cin>>num1;
+			cin>>num2;
+
+			cout<<"\nThe Greatest Common Divisor between "<<num1<<" and "<<num2<<" is: "<<gcd(num1,num2);
+		}
+		else if(option == 3)
+		{
+			auto choice = 'n';
+			cout<<"Are you sure you want to exit? (Y/N): ";
+			cin>>choice;
+
+			if(choice == 'y' || choice == 'Y')
+			{
+				cout<<"\nExiting program...\n";
+			}
+			else
+			{
+				cout<<"\nReturning to main menu...\n";
+				option = 0;
+			}
+		}
+
+    } while (option != 3);
 	return 0;
 }
