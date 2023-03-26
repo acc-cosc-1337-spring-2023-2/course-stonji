084a751c07269408aaa45f273174c3ba0117abb4
stonji <stonji@me.com>
2023-03-25 22:30:21 -0500

    bank account

--- src/examples/08_classes/01_bank/checking_account.h
+++ src/examples/08_classes/01_bank/checking_account.h
@@ -1,15 +1,15 @@
 //checking_account.h
-#include <stdlib.h>//for rand() function
+#include<iostream>
+#include "bank_account.h"
 
-class CheckingAccount
+#ifndef CHECKING_ACCOUNT_H
+#define CHECKING_ACCOUNT_H
+
+class CheckingAccount: public BankAccount//inherit from BankAccount
 {
-
 public:
-    CheckingAccount(){get_balance_from_db();}                           //constructor
-    CheckingAccount(int b) : balance(b){/*empty function code block*/}  //constructor
-    int get_balance() const {return balance;}
-    void deposit(int amount);
-private:
-    int balance{0};//explicitily initialize to zero
-    void get_balance_from_db();
+    CheckingAccount(){};//default constructor
+    CheckingAccount(int b) : BankAccount(b){}
 };
+
+#endif
--- src/examples/08_classes/01_bank/main.cpp
+++ src/examples/08_classes/01_bank/main.cpp
@@ -1,18 +1,22 @@
 #include<iostream>
 #include<time.h>
+#include<vector>
 #include "checking_account.h"
+#include "atm.h"
+#include "savings_account.h"
 
-using std::cout;
+using std::cout; using std::cin; using std::vector;
 
 int main()
 {
 	srand(time(NULL));//generate true randoms on each main run execution
+	vector<BankAccount*> accounts;
+	SavingsAccount savings;
+	CheckingAccount checking;
+	accounts.push_back(&checking);
+	accounts.push_back(&savings);
 
-	CheckingAccount account;//cust 1
-	cout<<"Balance: "<<account.get_balance()<<"\n";
-
-	CheckingAccount account1(100);//cust 2
-	cout<<"Balance: "<<account1.get_balance()<<"\n";
+	run_menu(accounts);
 
 	return 0;
 }
